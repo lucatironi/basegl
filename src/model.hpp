e104366f1c65cce850e7b9a29b567a78d5aba427
Luca Tironi <luca.tironi@gmail.com>
2018-11-04 20:36:43 +0100

    Remove useless namespace

--- src/model.hpp
+++ src/model.hpp
@@ -159,7 +159,7 @@ class Model
             vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
             textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
             // 3. normal maps
-            std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
+            vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
             textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
 
             // return a mesh object created from the extracted mesh data
@@ -179,7 +179,7 @@ class Model
                 bool skip = false;
                 for(unsigned int j = 0; j < textures_loaded.size(); j++)
                 {
-                    if(std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
+                    if(strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
                     {
                         textures.push_back(textures_loaded[j]);
                         skip = true; // a texture with the same filepath has already been loaded, continue to next one. (optimization)
@@ -235,7 +235,7 @@ class Model
         }
         else
         {
-            std::cout << "Texture failed to load at path: " << path << std::endl;
+            cout << "Texture failed to load at path: " << path << endl;
             stbi_image_free(data);
         }
 
